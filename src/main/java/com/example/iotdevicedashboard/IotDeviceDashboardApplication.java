03c1e9b5b555204fb6c33aa372a6686ba732fb00
athina <athinamannaraprayil@gmail.com>
2021-05-17 16:50:52 +0200

    Initial commit

---
+++ src/main/java/com/example/iotdevicedashboard/IotDeviceDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.iotdevicedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDeviceDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotDeviceDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iotdevicedashboard/IotDeviceDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotdevicedashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDeviceDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
