a57691ab9b91f8d71824a5fb8935d4b082f02216
athina <athinamannaraprayil@gmail.com>
2021-05-21 16:12:39 +0200

    changed to controller

--- src/main/java/com/example/iotdevicedashboard/controllers/RESTController.java
+++ src/main/java/com/example/iotdevicedashboard/controllers/RESTController.java
@@ -2,21 +2,27 @@ package com.example.iotdevicedashboard.controllers;
 
 import com.example.iotdevicedashboard.Models.deviceData;
 import com.example.iotdevicedashboard.repositories.DeviceDataDAO;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.List;
 
-@RestController
+@Controller
 public class RESTController {
     DeviceDataDAO data = new DeviceDataDAO();
 
+    /*@GetMapping("/home")
+    public String welcome(){
+        return "index";
+    }*/
+
     @RequestMapping("/deviceData")                 //http://localhost:8080/deviceData
-    public List<deviceData> getAllData() {
-        return data.getAllData();
+    public String  getAllData(Model model) {
+        List<deviceData>allData=data.getAllData();
+        model.addAttribute("index",allData);
+        return "index";
     }
 
     @RequestMapping("deviceData/add")      //http://192.168.0.243:8080/deviceData/add
@@ -31,9 +37,8 @@ public class RESTController {
         return ("added");
     }
 
-    @RequestMapping("deviceData/forecast")
+    @RequestMapping("deviceData/forecast")      //http://192.168.0.243:8080/deviceData/forecast
     public String TodaysForecast() {
-
         return data.Forecast();
     }
 
