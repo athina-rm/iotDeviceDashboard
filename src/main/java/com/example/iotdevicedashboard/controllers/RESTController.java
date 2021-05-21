9843fe75cedfe63572d17f676c632b86e6559d65
athina <athinamannaraprayil@gmail.com>
2021-05-21 16:59:57 +0200

    index trial1

--- src/main/java/com/example/iotdevicedashboard/controllers/RESTController.java
+++ src/main/java/com/example/iotdevicedashboard/controllers/RESTController.java
@@ -19,14 +19,13 @@ public class RESTController {
     }*/
 
     @RequestMapping("/deviceData")                 //http://localhost:8080/deviceData
-    public String  getAllData(Model model) {
-        List<deviceData>allData=data.getAllData();
-        model.addAttribute("index",allData);
+    public String  getAllData(deviceData d,Model model) {
+        model.addAttribute("allData",data.getAllData());
         return "index";
     }
 
     @RequestMapping("deviceData/add")      //http://192.168.0.243:8080/deviceData/add
-    public String addFriend(@RequestBody deviceData d) {
+    public String addData(@RequestBody deviceData d) {
         boolean status;
         data.addData(d);
         //System.out.println("deviceId:"+d.getDeviceId()+" Humidity:"+d.getHumidity()+" Temperature : "+d.getTemp()+" timestamp:"+d.getTimestamp()+" Time:"+(new Date(d.getTimestamp()*1000))+ d.getTime());
